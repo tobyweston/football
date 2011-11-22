619d258800e3258902facc1e03778e5d1076144e
Toby Weston <toby.weston@gmail.com>
2011-11-22 11:00:44 +0000

    story 1 (implemented)

---
+++ src/main/java/bad/robot/football/Result.java
@@ -0,0 +1,39 @@
+package bad.robot.football;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Result {
+
+    private final Date date;
+    private final String homeTeam;
+    private final int homeScore;
+    private final String awayTeam;
+    private final int awayScore;
+
+    public Result(Date date, String homeTeam, int homeScore, String awayTeam, int awayScore) {
+        this.date = date;
+        this.homeTeam = homeTeam;
+        this.homeScore = homeScore;
+        this.awayTeam = awayTeam;
+        this.awayScore = awayScore;
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        return EqualsBuilder.reflectionEquals(this, that);
+    }
+
+    @Override
+    public String toString() {
+        return new SimpleDateFormat("dd/MM/yyyy").format(date) + " " + homeTeam + " " + +homeScore + "-" + awayScore + " " + awayTeam ;
+    }
+}
---
+++ src/test/java/bad/robot/football/UnmarshallerTest.java
@@ -0,0 +1,45 @@
+package bad.robot.football;
+
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class UnmarshallerTest {
+
+    private final Unmarshaller unmarshaller = new Unmarshaller();
+
+    @Test(expected = FileNotFoundException.class)
+    public void shouldThrowExceptionForMissingFile() throws IOException, ParseException {
+        unmarshaller.unmarshall("rubarb.foo");
+    }
+
+    @Test
+    public void shouldUnmarshall() throws IOException, ParseException {
+        List<Result> results = unmarshaller.unmarshall("scores.csv");
+        List<Result> expected = new ArrayList<Result>() {{
+            add(new Result(new SimpleDateFormat("dd/MM/yyyy").parse("10/10/2011"), "Bristol City", 4, "Arsenal", 2));
+            add(new Result(new SimpleDateFormat("dd/MM/yyyy").parse("10/10/2011"), "Brompton", 2, "West Smithfields", 4));
+            add(new Result(new SimpleDateFormat("dd/MM/yyyy").parse("11/10/2011"), "Tottenham", 2, "Aston Villa", 0));
+            add(new Result(new SimpleDateFormat("dd/MM/yyyy").parse("11/10/2011"), "Chelsea", 1, "Liverpool", 2));
+            add(new Result(new SimpleDateFormat("dd/MM/yyyy").parse("12/11/2011"), "Everton", 2, "Wolverhampton", 1));
+            add(new Result(new SimpleDateFormat("dd/MM/yyyy").parse("12/11/2011"), "Man City", 3, "Newcastle", 2));
+            add(new Result(new SimpleDateFormat("dd/MM/yyyy").parse("12/11/2011"), "Norwich", 1, "Arsenal", 2));
+            add(new Result(new SimpleDateFormat("dd/MM/yyyy").parse("14/11/2011"), "Stoke", 2, "QPR", 3));
+            add(new Result(new SimpleDateFormat("dd/MM/yyyy").parse("14/11/2011"), "Sunderland", 0, "Fulham", 1));
+            add(new Result(new SimpleDateFormat("dd/MM/yyyy").parse("15/11/2011"), "Swansea", 0, "Man Utd", 1));
+            add(new Result(new SimpleDateFormat("dd/MM/yyyy").parse("16/11/2011"), "West Brom", 2, "Bolton", 1));
+            add(new Result(new SimpleDateFormat("dd/MM/yyyy").parse("19/11/2011"), "Wigan", 3, "Blackburn", 3));
+        }};
+        assertThat(results, is(equalTo(expected)));
+    }
+
+}
