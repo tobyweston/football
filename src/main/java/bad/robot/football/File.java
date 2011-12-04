14357b3621e79f94dee18a5665df9444b4de972a
Toby Weston <toby.weston@gmail.com>
2011-12-04 17:30:50 +0000

    minor tweak

--- src/main/java/bad/robot/football/File.java
+++ src/main/java/bad/robot/football/File.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 
 public class File {
     public static InputStreamReader getFileAsReader(String filename) throws IOException {
-        InputStream stream = CsvUnmarshaller.class.getResourceAsStream(filename);
+        InputStream stream = File.class.getResourceAsStream(filename);
         if (stream == null)
             throw new FileNotFoundException(filename);
         return new InputStreamReader(stream);
